465a70b987cd6b77cf2fb06b4a256304102fd589
John Andrew Divina <98078051+LazyAndrew1333@users.noreply.github.com>
2022-09-01 15:29:11 +0800

    Create Item.java

---
+++ Week 3/Multiple_Class_With_Constructor/Item.java
@@ -0,0 +1,27 @@
+public class Item {
+    // Items Attributes
+    String name;
+    Double price;
+    Double weight;
+    int power;
+    int reliability;
+    String ammo;
+    String description;
+    Boolean tradeable;
+
+    // Items Method
+    public void itemDescription() {
+        System.out.println("Name: " + name + "\n" + "Price: " + price + "      | Weight: " + weight + "\n" + "Power: " + power + "           | Reliability: " + reliability + "\n" + "Ammo Type: " + ammo + " | Can be traded: " + tradeable + "\n" + "Description: " + description + "\n");
+    }
+
+    // Constructor
+    public Item(String itemName, Double itemPrice, Double itemWeight, int itemReliability, String itemAmmoType, String itemDescription, Boolean canBeTraded) {
+        name = itemName;
+        price = itemPrice;
+        weight = itemWeight;
+        reliability = itemReliability;
+        ammo = itemAmmoType;
+        description = itemDescription;
+        tradeable = canBeTraded;
+    }
+}
