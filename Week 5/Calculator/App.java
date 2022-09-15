c9fe634a3eb69d2e453045bdacb02a96ab427d02
John Andrew Divina <98078051+LazyAndrew1333@users.noreply.github.com>
2022-09-15 15:18:29 +0800

    Create App.java

---
+++ Week 5/Calculator/App.java
@@ -0,0 +1,42 @@
+// import the Scanner class
+import java.util.Scanner;
+
+public class App {
+
+    // Scanner Object
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Method call
+        int product = multiply();
+        System.out.println("The product is: " + product + "\n");
+
+        // Method call
+        int quotient = divide();
+        System.out.println("The quotient is: " + quotient + "\n");
+
+        // Close scanner
+        scan.close();
+    }
+
+    static int multiply() {
+        System.out.println("\nPlease enter two numbers to multiply");
+        System.out.print("Enter first number: ");
+        int x = scan.nextInt();
+
+        System.out.print("Enter second number: ");
+        int y = scan.nextInt();
+        return x * y;
+    }
+
+    static int divide() {
+        System.out.println("Please enter two numbers to divide");
+        System.out.print("Enter first number: ");
+        int x = scan.nextInt();
+
+        System.out.print("Enter second number: ");
+        int y = scan.nextInt();
+        return x / y;
+    }
+}
